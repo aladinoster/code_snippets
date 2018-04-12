11eb8e7058287a2e99bdf8810bae4a0dceb148f8
Andres Ladino <ladino@Mac-mini-de-Andres.local>
2018-04-12 16:08:57 +0200

    Update on solving ODEs

--- Python-Andres-Scripts/odesolve.py
+++ Python-Andres-Scripts/odesolve.py
@@ -9,10 +9,10 @@ def test(x,t):
     return [x[1],-x[1]+0.2-x[0]]
 
 
-t = np.linspace(0,50,1000)
-u = 10*np.sin(2*3.1415*t)
+tx = np.linspace(0,50,1000)
+u = 10*np.sin(2*3.1415*tx)
 y0 = np.array([10, 10])
-sol = odeint(test,y0,t)
+sol = odeint(test,y0,tx)
 
 # plt.plot(t,sol)
 # plt.show()
@@ -21,7 +21,7 @@ i = 0
 tp = np.empty([1,1])
 y = np.zeros([2,1])
 y[:,0] = [10,10]
-for a,b in zip(t,u):
+for a,b in zip(tx,u):
     if i == 1:
         tint = [tp, a]
         sol = odeint(test_u,y[:,-1],tint,args = (b,))
@@ -30,5 +30,27 @@ for a,b in zip(t,u):
     i = 1
 
 
-plt.plot(y.T)
+## This may be the best alternative, considering that it is completely numpy based and it supports the size of the vector of time as a reference, initial condition can be freely asigned from 
+
+n_it = len(tx)+1
+y = np.zeros([2,n_it])  
+y[:,0] = np.array([10,10])
+tp = tx[0]
+y0 = y[:,0]
+i = 1
+for t, a in zip(tx,u):
+    if t != tp: 
+        t_int = [tp, t]            
+        y_sol = odeint(test_u,y0,t_int,args=(a,))            
+        y[:,i] = y_sol[-1]
+        y0 = y_sol[-1]
+    i+=1
+    tp = t
+
+y = y[:,:-1]
+
+print(y.shape)
+
+plt.plot(tx,y[1,])
+plt.plot(tx,y[0,])
 plt.show()
