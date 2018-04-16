105dd5845ae94bb22872c11772c957cb09eab3af
Andres Ladino <ladino@Mac-mini-de-Andres.local>
2018-04-16 10:15:29 +0200

    Repr methods python

---
+++ Python-Andres-Scripts/reprmethods.py
@@ -0,0 +1,13 @@
+class Car: 
+    def __init__(self, color, mileage):
+        self.color = color
+        self.mileage = mileage
+
+    def __str__(self):
+        return 'a {self.color} car'.format(self=self)
+
+    def __repr__(self):
+        return '{self.__class__.__name__}({self.color}, {self.mileage})'.format(self=self)
+
+if __name__ == '__main__':
+    my_car = Car('red', 2)
