c6bab514fc8e6e05057286a4fb7bb4b5ea2798b5
Andres Ladino <andres.ladino@ifsttar.fr>
2018-07-10 12:56:03 +0200

    Update launcher zip

--- Python-packaging/Example_launcher/main.py
+++ Python-packaging/Example_launcher/main.py
@@ -1,3 +1,8 @@
 ## Launch python3 reader test.bz2
 ## Launch python3 reader test.gz
 ## Launch python3 reader reader/__main__.py 
+## 
+## Create a compressed file by doing 
+# cd reader
+# zip -r ../reader.zip *  # Creates the zip in the original root directory same as path as reader 
+# python3 reader.zip test.gz
