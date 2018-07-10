eede8e73dc32f9e6e47d5d70454093c6f3f2fc81
Andres Ladino <andres.ladino@ifsttar.fr>
2018-07-10 12:51:34 +0200

    Adding an interface for users

---
+++ Python-packaging/Example_launcher/main.py
@@ -0,0 +1,3 @@
+## Launch python3 reader test.bz2
+## Launch python3 reader test.gz
+## Launch python3 reader reader/__main__.py 
---
+++ Python-packaging/Example_launcher/reader/reader/__init__.py
@@ -0,0 +1,5 @@
+# Absolute import 
+from reader.reader import Reader
+
+# relative import 
+from .reader import Reader 
