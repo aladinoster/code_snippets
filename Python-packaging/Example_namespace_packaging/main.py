30b8227f8012503df218d1545755294582b24a60
Andres Ladino <andres.ladino@ifsttar.fr>
2018-07-10 12:41:19 +0200

    Imports detailed

--- Python-packaging/Example_namespace_packaging/main.py
+++ Python-packaging/Example_namespace_packaging/main.py
@@ -3,3 +3,9 @@ sys.path.extend(['path_1', 'path_2'])
 
 import split_farm
 print(split_farm.__path__)
+
+import split_farm.bird
+import split_farm.bovine
+
+print(split_farm.bird.__path__)
+print(split_farm.bovine.__path__)
