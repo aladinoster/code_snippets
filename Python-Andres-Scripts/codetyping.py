cd6d14ae989dc8780159f707d7ba61617202c308
Andres Ladino <aladinoster@gmail.com>
2018-11-09 10:54:24 +0100

    Add code typing

---
+++ Python-Andres-Scripts/codetyping.py
@@ -0,0 +1,20 @@
+from typing import NewType
+import sys
+print(sys.version)
+
+UserId = NewType('UserId', int)
+some_id = UserId(524313)
+
+
+def get_user_name(user_id: UserId):  # -> str:
+    print(f'Getting {user_id}')
+
+
+def get_another_user(user_id: UserId)->str:
+    print(f'Getting {user_id}')
+
+
+if __name__ == "__main__":
+    some_id = UserId(524313)
+    get_user_name(some_id)
+    get_another_user(some_id)
