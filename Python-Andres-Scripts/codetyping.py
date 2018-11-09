a81c5a5a6d81ce15f24f9fd610e3d7ad79745d8a
Andres Ladino <aladinoster@gmail.com>
2018-11-09 13:41:49 +0100

    Studying syntax highlighting error tht13/Python-vscode #27

--- Python-Andres-Scripts/codetyping.py
+++ Python-Andres-Scripts/codetyping.py
@@ -6,12 +6,13 @@ UserId = NewType('UserId', int)
 some_id = UserId(524313)
 
 
-def get_user_name(user_id: UserId):  # -> str:
+def get_user_name(user_id: UserId):
     print(f'Getting {user_id}')
 
 
 def get_another_user(user_id: UserId)->str:
     print(f'Getting {user_id}')
+    return user_id
 
 
 if __name__ == "__main__":
